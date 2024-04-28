2d4a0a13fa1a3093c124b5877c311298f93e14d7
Rafaela Pessoa <rafaela_tunu@hotmail.com>
2024-04-28 23:42:56 +0200

    creation funcion render and handleclick

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,40 @@
 'use strict';
 
-let coctailData = [];
+const ulList = document.querySelector(".js_cocktails");
+const inputSearch = document.querySelector(".js_input");
+const buttonSearch = document.querySelector(".js_btn_search");
+const buttonReset = document.querySelector(".js_btn_reset");
 
-const renderOneCoctails =()=>{
-    
-}
 
-const renderAllCoctails =()=>{
+let drinksData = [];
 
-}
+function renderCocktails (list){
+    ulList.innerHTML = '';
+    for (const eachCocktail of list){
+        ulList.innerHTML += `<li> 
+        <h4> ${eachCocktail.strDrink} </h4>
+        <img src = "${eachCocktail.strImageSource}"/>
+        </li>`
+    }    
+};
 
-const getData = ()=>{
+function getData (){
     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
     .then((response)=> response.json())
     .then((dataApi)=>{
-        coctailData = dataApi.driks;
-        console.log(dataApi);
+        drinksData = dataApi.drinks;
+        console.log(drinksData);
+        renderCocktails(drinksData);
     });
 };
 
+const handleSearch = () => {
+    const valueSearch = inputSearch.value;
+    
+    const filteredCocktail = drinksData.filter((item) => item.strDrink.toLowerCase().includes(valueSearch.toLowerCase()));
+    renderCocktails(filteredCocktail);
+    console.log(filteredCocktail);
+};
+
+inputSearch.addEventListener("input", handleSearch);
 getData();
