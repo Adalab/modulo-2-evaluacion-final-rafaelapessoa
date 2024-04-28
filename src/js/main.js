b84f717fc5070420de1d8795c3ae5479619b5955
Rafaela Pessoa <rafaela_tunu@hotmail.com>
2024-04-28 12:14:30 +0200

    render

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
+let coctailData = [];
 
+const renderOneCoctails =()=>{
+    
+}
+
+const renderAllCoctails =()=>{
+
+}
+
+const getData = ()=>{
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+    .then((response)=> response.json())
+    .then((dataApi)=>{
+        coctailData = dataApi.driks;
+        console.log(dataApi);
+    });
+};
+
+getData();
