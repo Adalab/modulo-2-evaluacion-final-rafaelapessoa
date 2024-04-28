4548f2ea21be695ef4743a3f72236bc9714431f4
Rafaela Pessoa <rafaela_tunu@hotmail.com>
2024-04-29 00:23:55 +0200

    list facorite

--- src/js/main.js
+++ src/js/main.js
@@ -7,15 +7,24 @@ const buttonReset = document.querySelector(".js_btn_reset");
 
 
 let drinksData = [];
+let drinksFavorites = [];
+
+const addFavorites = (ev) => {
+    console.log(ev.currentTarget.idDrink);
+};
 
 function renderCocktails (list){
     ulList.innerHTML = '';
     for (const eachCocktail of list){
-        ulList.innerHTML += `<li> 
+        ulList.innerHTML += `<li class = "js_list_cocktails" id= "${eachCocktail.idDrink}"> 
         <h4> ${eachCocktail.strDrink} </h4>
         <img src = "${eachCocktail.strImageSource}"/>
         </li>`
-    }    
+    }
+    const allDrinksLi = document.querySelectorAll(".js_list_cocktails");
+    for (const li of allDrinksLi) {
+        li.addEventListener("click", addFavorites);        
+    }
 };
 
 function getData (){
