1160d0b77e8a805eb25e97382fa4f95dbcb3b688
Rafaela Pessoa <rafaela_tunu@hotmail.com>
2024-04-29 11:26:13 +0200

    list favorites

--- src/js/main.js
+++ src/js/main.js
@@ -10,18 +10,40 @@ let drinksData = [];
 let drinksFavorites = [];
 
 const addFavorites = (ev) => {
-    console.log(ev.currentTarget.idDrink);
+    console.log(ev.currentTarget.id);
+    //obtener datos de la paleta clicada
+    const liClikedId = ev.currentTarget.id;
+    const clikedDrinkData = drinksData.find((item) => item.idDrink === liClikedId);
+    //item.idDrink es el valor que va a buscar dentro del array
+
+    //verificar si el drink clicado ya es un favorito
+    const favoriteLiClickedIndex = drinksFavorites.findIndex((item) => item.idDrink === liClikedId);
+
+    if(favoriteLiClickedIndex === -1){
+        //add al array de fav el drink clicado, si ya no esta
+        drinksFavorites.push(clikedDrinkData);
+    } else{
+        //quitar del array de fav
+        drinksFavorites.splice(favoriteLiClickedIndex, 1);
+    }       
+    console.log(drinksFavorites);
+    renderCocktails(drinksData);    
 };
 
 function renderCocktails (list){
     ulList.innerHTML = '';
+    const indexFav = drinksFavorites.find(item => item.idDrink === eachCocktail.idDrink);
+
+    let classCss = indexFav === -1 ? '' : 'blue';
+
     for (const eachCocktail of list){
-        ulList.innerHTML += `<li class = "js_list_cocktails" id= "${eachCocktail.idDrink}"> 
-        <h4> ${eachCocktail.strDrink} </h4>
-        <img src = "${eachCocktail.strImageSource}"/>
+        ulList.innerHTML += `<li class = "js_list_cocktails ${classCss}" id= "${eachCocktail.idDrink}">         
+        <h3> ${eachCocktail.strDrink} </h3>
+        <img class="photo" src = "${eachCocktail.strDrinkThumb}"/>
         </li>`
     }
-    const allDrinksLi = document.querySelectorAll(".js_list_cocktails");
+    
+    const allDrinksLi = document.querySelectorAll('.js_list_cocktails');
     for (const li of allDrinksLi) {
         li.addEventListener("click", addFavorites);        
     }
